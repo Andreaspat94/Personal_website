6fd7aba0b7ea37eb98db2053116b7037bd370080
Andreas Paterakis <andreas_pat@hotmail.gr>
2023-03-21 17:32:13 +0200

    last commit

---
+++ credits.js
@@ -0,0 +1,13 @@
+const heart = document.getElementById('heart');
+
+const likeUnlikePost = function() {
+    if(heart.classList.contains('like')) {
+        heart.classList.remove('like');
+        heart.classList.add('unlike');
+    } else {
+        heart.classList.remove('unlike');
+        heart.classList.add('like');
+    }
+}
+
+heart.addEventListener('click', likeUnlikePost);
